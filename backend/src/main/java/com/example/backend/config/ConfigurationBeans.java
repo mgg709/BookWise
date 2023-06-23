1da610a5d06b98bd10ab10a0f2c99ca4b453f5cb
Matias Garcia <mggimeno11@gmail.com>
2023-06-23 12:10:33 +0200

    test

--- backend/src/main/java/com/example/backend/config/ConfigurationBeans.java
+++ backend/src/main/java/com/example/backend/config/ConfigurationBeans.java
@@ -1,2 +1,25 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import com.example.backend.model.Book;
+
+
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.example.config")
+public class ConfigurationBeans extends ElasticsearchConfiguration{
+
+    @Override
+    public ClientConfiguration clientConfiguration(){
+        return ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .build();
+    }
+    
+    @Bean
+    public Book book() {
+        return new Book();
+    }
+}
