648c14238fab71238588c4daf4d3d3b60909f13a
Matias Garcia <mggimeno11@gmail.com>
2023-06-30 11:10:20 +0200

    elastic with index working

--- backend/src/main/java/com/example/backend/config/ConfigurationBeans.java
+++ backend/src/main/java/com/example/backend/config/ConfigurationBeans.java
@@ -1,11 +1,16 @@
 package com.example.backend.Config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
+import com.example.backend.model.Book;
+
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.example.backend.repositories")
+@ComponentScan(basePackages = { "com.example.backend" })
 public class ConfigurationBeans extends ElasticsearchConfiguration{
 
     @Override
--- backend/src/main/java/com/example/backend/repositories/BookRepository.java
+++ backend/src/main/java/com/example/backend/repositories/BookRepository.java
@@ -3,10 +3,12 @@ package com.example.backend.Repositories;
 import java.util.List;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.Repository;
+import com.example.backend.model.Book;
 
-import com.example.backend.Model.Book;
 
-
-public interface BookRepository extends ElasticsearchRepository<Book, String> {
+@org.springframework.stereotype.Repository
+public interface BookRepository extends ElasticsearchRepository<Book, Integer>{
+    List<Book> findByTitle(String title);
     public List<Book> findAll();
 }
