2b1671c3464da6679861c5fef155491e44fe99ed
LMBaizan <132277472+LMBaizan@users.noreply.github.com>
2023-07-04 11:00:42 +0200

    Profile

---
+++ frontend/bookwise-front/src/views/Profile.view.vue
@@ -0,0 +1,145 @@
+<script setup>
+import Header from "../components/ProfileHeader.vue"
+</script>
+
+<template>
+    <Header imageName="search"></Header>
+    <section>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>
+        <p>ESTO ES UNA PRUEBA</p>   
+    </section>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
