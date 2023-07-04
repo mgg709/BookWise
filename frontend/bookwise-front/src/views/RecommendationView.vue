788bc0bcc24afad7fd721bb3ab88d5e9a919c8f1
Matias Garcia <mggimeno11@gmail.com>
2023-07-04 12:23:12 +0200

    recommendation view only design and routing

---
+++ frontend/bookwise-front/src/views/RecommendationView.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="description">Description of the section</span>
+    <div class="question">
+        <span>What are your favourite genres?</span>
+        <input type="text">
+    </div>
+</template>
+<script>
+</script>
+<style>
+.description{
+    margin: 20px;
+}
+.question{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid #ff9153 ;
+    padding: 20px;
+    border-radius: 7px;
+    & span{
+        margin-bottom: 20px;
+    }
+}
+</style>
